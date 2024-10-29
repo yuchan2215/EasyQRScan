398b4fc6405799d1c4972fbeedd1b80dc70b4b47
yuchan2215 <yuchan2215@gmail.com>
2024-10-29 12:20:42 +0900

    add: publishできるように

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.compose.internal.utils.getLocalProperty
-import java.net.URI
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -9,21 +6,23 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kmp) apply false
     alias(libs.plugins.dokka)
-    alias(libs.plugins.nexusPublish)
+    id("maven-publish")
 }
 
 subprojects {
     group = "io.github.kalinjul.easyqrscan"
 }
 
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(URI("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(URI("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(getLocalProperty("OSSRH_USERNAME") ?: System.getenv("OSSRH_USERNAME"))
-            password.set(getLocalProperty("OSSRH_PASSWORD") ?: System.getenv("OSSRH_PASSWORD"))
-            stagingProfileId.set(getLocalProperty("SONATYPE_STAGING_PROFILE_ID") ?: System.getenv("SONATYPE_STAGING_PROFILE_ID"))
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "jp.miyayu.easyqrscan"
+                artifactId = "easyqrscan"
+                version = "0.0.1"
+            }
         }
     }
 }
